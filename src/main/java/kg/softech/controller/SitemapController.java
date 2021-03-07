9de31c8ff7b3d5cbf8128abbb7eb4a7b44b36b05
kb1prb13 <dastan.namazov.96@gmail.com>
2021-03-07 18:19:03 +0100

    Fixed sitemap retrieving

--- src/main/java/kg/softech/controller/SitemapController.java
+++ src/main/java/kg/softech/controller/SitemapController.java
@@ -4,59 +4,56 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.w3c.dom.Document;
-
 
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import static kg.softech.config.globalvariable.GlobalVar.SITEMAP;
+
 @Controller
 public class SitemapController {
 
+  @GetMapping("/sitemap.xml")
+  @ResponseBody
+  public FileSystemResource getSitemap(HttpServletResponse responseFirst) {
+    FileOutputStream fos;
 
-    @GetMapping("/sitemap.xml")
-    @ResponseBody
-    public FileSystemResource getSitemap(HttpServletResponse responseFirst){
-        FileOutputStream fos = null;
-        try {
-            // Возьмите файл
-            File f = new File("C:\\Users\\Dosmir\\Desktop\\sitemap.xml");
-            //Создайте новый файл
-            // Убедитесь, что он не существует
-            if (f.createNewFile())
-                System.out.println("File created");
-            else
-                System.out.println("File already exists");
+    // Возьмите файл            File f = new File("C:\\Users\\Dosmir\\Desktop\\sitemap.xml");
+    File f = new File(SITEMAP);
 
-            //Получение сайтмэп
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("http://web.imd.kg:8080/getSiteMap?baseUrl=https://www.emin.kg/product/&storeId=5"))
+    // Получение сайтмэп
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request =
+            HttpRequest.newBuilder()
+                    .uri(
+                            URI.create(
+                                    "http://web.imd.kg:8080/getSiteMap?baseUrl=https://www.emin.kg/product/&storeId=5"))
                     .build();
 
-            HttpResponse<String> response =
-                    client.send(request, HttpResponse.BodyHandlers.ofString());
-            /*System.out.println(response.body());*/
+    try {
+      HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            //Запись в файл
-            //String fileData = "Досмир красавчик";
-            fos = new FileOutputStream("C:\\Users\\Dosmir\\Desktop\\sitemap.xml");
-            fos.write(response.body().getBytes());
-            fos.flush();
-            fos.close();
-        }
-        catch (Exception e) {
-            System.err.println(e);
-        }
-        responseFirst.setContentType("application/xml");
-        return new FileSystemResource(new File("C:\\Users\\Dosmir\\Desktop\\sitemap.xml"));
+      // Создайте новый файл
+      // Убедитесь, что он не существует
+      if (f.createNewFile()) {
+        // Запись в файл        fos = new FileOutputStream("C:\\Users\\Dosmir\\Desktop\\sitemap.xml");
+        fos = new FileOutputStream(SITEMAP);
+        fos.write(response.body().getBytes());
+        fos.flush();
+        fos.close();
+      }
+    } catch (IOException | InterruptedException e) {
+      System.err.println(e);
     }
+
+    responseFirst.setContentType("application/xml");
+    //        return new FileSystemResource(new File("C:\\Users\\Dosmir\\Desktop\\sitemap.xml"));
+    return new FileSystemResource(new File(SITEMAP));
+  }
 }
