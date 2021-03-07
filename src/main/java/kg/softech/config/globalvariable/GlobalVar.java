f73008ad16d859c2b0d99e1ca0d1e0a2476f2581
kb1prb13 <dastan.namazov.96@gmail.com>
2021-03-07 11:01:56 +0100

    Edited files paths and store id

--- src/main/java/kg/softech/config/globalvariable/GlobalVar.java
+++ src/main/java/kg/softech/config/globalvariable/GlobalVar.java
@@ -2,16 +2,21 @@ package kg.softech.config.globalvariable;
 
 public class GlobalVar {
 
-//    public static int storeId = 1;
-    public static int storeId = 1;
-    //    public static String mainURL="http://web.imd.kg:8080/";
-    public static String mainURL = "http://212.2.230.77:8080/";
-    public static String storeName = "EMIN.KG";
+  public static int storeId = 5;
 
-    // Linux: /home/files/    Windows: C:\home\files\
-    public static final String SITEMAP = "/home/files/eminkg";
+  //    public static String mainURL="http://web.imd.kg:8080/";
+  public static String mainURL = "http://212.2.230.77:8080/";
+  public static String storeName = "EMIN.KG";
 
-    // Linux: /home/banners/    Windows: C:\home\banners\
-    public static final String BANNER_UPLOAD_DIRECTORY = "/home/banners/eminkg";
+  // Linux: System.getProperty("user.home") + "/files/eminkg/"
+  // Windows: System.getProperty("user.home") + "\\files\\eminkg\\"
+  public static final String SITEMAP = System.getProperty("user.home") + "/files/eminkg/sitemap.xml";
 
+  // Linux: System.getProperty("user.home") + "/banners/eminkg/"
+  // Windows: System.getProperty("user.home") + "\\banners\\eminkg\\"
+  public static final String BANNER_UPLOAD_DIRECTORY = System.getProperty("user.home") + "/banners/eminkg/";
+
+  // Linux: System.getProperty("user.home") + "/files/eminkg/"
+  // Windows: System.getProperty("user.home") + "\\files\\eminkg\\"
+  public static final String FAVICON = System.getProperty("user.home") + "/files/eminkg/favicon.ico";
 }
