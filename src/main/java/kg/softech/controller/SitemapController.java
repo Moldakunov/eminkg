d55664ba696671f6ef9b078135e5f16ae11d6fab
kb1prb13 <dastan.namazov.96@gmail.com>
2021-03-08 19:32:30 +0100

    Added feature generating sitemap every 10 minutes after last modify

--- src/main/java/kg/softech/controller/SitemapController.java
+++ src/main/java/kg/softech/controller/SitemapController.java
@@ -1,5 +1,11 @@
 package kg.softech.controller;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +15,15 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
-import static kg.softech.config.globalvariable.GlobalVar.SITEMAP;
+import static kg.softech.config.globalvariable.GlobalVar.*;
 
 @Controller
 public class SitemapController {
@@ -24,31 +33,41 @@ public class SitemapController {
   public FileSystemResource getSitemap(HttpServletResponse responseFirst) {
     FileOutputStream fos;
 
-    // Возьмите файл            File f = new File("C:\\Users\\Dosmir\\Desktop\\sitemap.xml");
-    File f = new File(SITEMAP);
-
-    // Получение сайтмэп
-    HttpClient client = HttpClient.newHttpClient();
-    HttpRequest request =
-            HttpRequest.newBuilder()
-                    .uri(
-                            URI.create(
-                                    "http://web.imd.kg:8080/getSiteMap?baseUrl=https://www.emin.kg/product/&storeId=5"))
-                    .build();
+    Path file = Paths.get(SITEMAP);
+    BasicFileAttributes attributes;
 
     try {
-      HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+      attributes = Files.readAttributes(file, BasicFileAttributes.class);
 
-      // Создайте новый файл
-      // Убедитесь, что он не существует
-      if (f.createNewFile()) {
-        // Запись в файл        fos = new FileOutputStream("C:\\Users\\Dosmir\\Desktop\\sitemap.xml");
-        fos = new FileOutputStream(SITEMAP);
-        fos.write(response.body().getBytes());
+      FileTime lastModifiedTime = attributes.lastModifiedTime();
+      LocalDateTime modifiedTime =
+          LocalDateTime.ofInstant(lastModifiedTime.toInstant(), ZoneId.systemDefault());
+
+      LocalDateTime now = LocalDateTime.now();
+
+      if (modifiedTime.plusMinutes(10).isBefore(now)) {
+        // Получение сайтмэп
+        CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
+
+        HttpGet request =
+            new HttpGet(
+                mainURL + "getSiteMap?baseUrl=https://www.emin.kg/product/&storeId=" + storeId);
+
+        CloseableHttpResponse response = closeableHttpClient.execute(request);
+        HttpEntity entity = response.getEntity();
+
+        // Перезаписываем уже существующий файл, т.к. он уже устарел
+        // флаг false означает перезапись файла
+        fos = new FileOutputStream(SITEMAP, false);
+        fos.write(EntityUtils.toByteArray(entity));
         fos.flush();
         fos.close();
+
+        System.out.println("о, 10 минут прошли, ГЕНЕРИМ новый sitemap!!!");
+      } else if (modifiedTime.plusMinutes(10).isAfter(now)) {
+        System.out.println("еще не прошли 10 минут, так что довольствуйся тем, что есть дорогой");
       }
-    } catch (IOException | InterruptedException e) {
+    } catch (IOException e) {
       System.err.println(e);
     }
 
